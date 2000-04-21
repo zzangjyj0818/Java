9c0d4158573b7578c128a6b76a844148db9b6131
zzangjyj0818 <jyj010818@naver.com>
2000-04-21 10:00:00 +0000

    oop Study

--- intelij-workspace/Syntax/src/CallbackTest.java
+++ intelij-workspace/Syntax/src/CallbackTest.java
@@ -1,16 +1,29 @@
 import javax.swing.Timer;
 import java.awt.event.ActionListener;
 
+//public class CallbackTest {
+//    public static void main(String[] args) {
+//        ActionListener listener = new MyClass();
+//        Timer t = new Timer(1000, listener);
+//        t.start();
+//        for(int i = 0; i<5; i++){
+//            try{
+//                Thread.sleep(1000);
+//            }catch (InterruptedException e){
+//            }
+//        }
+//    }
+//}
+
 public class CallbackTest {
     public static void main(String[] args) {
-        ActionListener listener = new MyClass();
-        Timer t = new Timer(1000, listener);
+        Timer t = new Timer(1000, event-> System.out.println("beep"));
         t.start();
-        for(int i = 0; i<1000; i++){
+
+        for(int i = 0; i<3; i++){
             try{
                 Thread.sleep(1000);
-            }catch (InterruptedException e){
-            }
+            }catch (InterruptedException e){}
         }
     }
 }
