c1b59e3a1d86998cdbaf1860d46218286d85c1cd
Jeong Yeon Jae <jyj010818@naver.com>
2023-03-23 10:24:17 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Class/BoxTest.java
@@ -0,0 +1,13 @@
+package Class;
+
+public class BoxTest {
+    public static void main(String[] args) {
+        Box obj1 = new Box(10,20,50);
+        Box obj2 = new Box(10,20,60);
+
+        if(obj1.isSameBox(obj2))
+            System.out.println("obj1 == obj2");
+        else
+            System.out.println("obj1 != obj2");
+    }
+}
--- intelij-workspace/OOP_Study/src/Class/Television.java
+++ intelij-workspace/OOP_Study/src/Class/Television.java
@@ -1,2 +1,18 @@
-package Class;public class Television {
+package Class;
+
+public class Television {
+    private int channel;
+    private int volume;
+    private boolean onOff;
+
+
+    public Television(int channel, int volume, boolean onOff) {
+        this.channel = channel;
+        this.volume = volume;
+        this.onOff = onOff;
+    }
+
+    void print(){
+        System.out.println("채널은 " + channel +"이고 볼륨은 " + volume + "입니다.");
+    }
 }
