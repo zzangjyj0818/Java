80f1bac7118bae205e1430aabca5bab15ab6ede3
Jeong Yeon Jae <jyj010818@naver.com>
2023-04-05 23:39:13 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/BooleanTest.java
@@ -0,0 +1,10 @@
+public class BooleanTest {
+    public static void main(String[] args) {
+        boolean b;
+
+        b= true;
+        System.out.println("b : " + b);
+        b = (1 > 2);
+        System.out.println("b : " + b);
+    }
+}
