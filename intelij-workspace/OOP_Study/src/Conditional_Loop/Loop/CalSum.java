cbff6d8aedd423a082b3f05495976e0c647769f0
zzangjyj0818 <jyj010818@naver.com>
2023-04-09 15:20:07 +0900

    oop Study

--- intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/CalSum.java
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/CalSum.java
@@ -3,7 +3,7 @@ package Conditional_Loop.Loop;
 public class CalSum {
     public static void main(String[] args) {
         int i = 1, sum = 0;
-        while(i<10){
+        while(i<=10){
             sum += i;
             i++;
         }
---
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/DoWhile1.java
@@ -0,0 +1,11 @@
+package Conditional_Loop.Loop;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i = 10;
+        do{
+            System.out.println("i의 값 : " + i);
+            i++;
+        }while(i<3);
+    }
+}
