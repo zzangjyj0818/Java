f238426f4cab1f68be58532b2bccf5c7924f9e46
zzangjyj0818 <jyj010818@naver.com>
2023-04-09 16:15:56 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Chapter03/For/Sum.java
@@ -0,0 +1,12 @@
+package Chapter03.For;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i = 1; i<=10; i++)
+            sum += i;
+
+        System.out.println(sum);
+    }
+}
