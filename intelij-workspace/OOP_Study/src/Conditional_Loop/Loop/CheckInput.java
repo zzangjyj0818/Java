f67c1e203c6e8b799b098c4744a56fbbe9deddf3
zzangjyj0818 <jyj010818@naver.com>
2023-04-09 15:28:15 +0900

    oop Study

--- intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/CheckInput.java
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/CheckInput.java
@@ -1,2 +1,14 @@
-package Conditional_Loop.Loop;public class CheckInput {
+package Conditional_Loop.Loop;
+
+import java.util.*;
+
+public class CheckInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int month;
+        do {
+            System.out.print("올바른 월을 입력하시오 [1-12] : ");
+            month = input.nextInt();
+        } while (month < 1 || month > 12);
+    }
 }
---
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/Gcd.java
@@ -0,0 +1,21 @@
+package Conditional_Loop.Loop;
+
+import java.util.*;
+
+public class Gcd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x, y, r;
+        System.out.print("두 개의 정수를 입력하시오(큰수, 작은수) : ");
+        x = input.nextInt();
+        y = input.nextInt();
+
+        while (y != 0) {
+            r = x % y;
+            x = y;
+            y = r;
+        }
+        System.out.println(x);
+    }
+}
