e8b3c9e9cdebf8ec9a2a7a2eb9259f8585a16afe
zzangjyj0818 <jyj010818@naver.com>
2023-04-09 15:19:10 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/CalSum.java
@@ -0,0 +1,12 @@
+package Conditional_Loop.Loop;
+
+public class CalSum {
+    public static void main(String[] args) {
+        int i = 1, sum = 0;
+        while(i<10){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
