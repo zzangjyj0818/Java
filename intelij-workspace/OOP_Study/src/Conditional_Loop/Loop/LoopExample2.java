702aa8acebe7e300f1efb9a834e480058776f515
zzangjyj0818 <jyj010818@naver.com>
2023-04-09 15:17:30 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Conditional_Loop/Loop/LoopExample2.java
@@ -0,0 +1,17 @@
+package Conditional_Loop.Loop;
+
+import java.util.*;
+
+public class LoopExample2 {
+    public static void main(String[] args) {
+        int n, i = 1;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("구구단 중에서 출력하고 싶은 단을 입력하세요 : ");
+        n = scan.nextInt();
+        while(i<=9){
+            System.out.printf("%d * %d = %d\n", n, i, n*i);
+            i++;
+        }
+    }
+}
