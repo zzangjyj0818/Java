65a86978867bae3651d42f586f77642aa4b62941
zzangjyj0818 <jyj010818@naver.com>
2023-04-10 17:26:43 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Chapter06/AccessControl/SafeArrayTest.java
@@ -0,0 +1,10 @@
+package Chapter06.AccessControl;
+
+public class SafeArrayTest {
+    public static void main(String[] args) {
+        SafeArray array = new SafeArray(3);
+
+        for(int i = 0; i<array.length+1; i++)
+            array.put(i,i*10);
+    }
+}
