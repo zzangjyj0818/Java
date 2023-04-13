abff4df13d92b2f05b31ce3f2e59e689509a6396
zzangjyj0818 <jyj010818@naver.com>
2023-04-13 09:47:12 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Lecture/Manager.java
@@ -0,0 +1,18 @@
+package Lecture;
+
+public class Manager extends Employee{
+    private int bonus;
+
+    public Manager(String name, String address, int salary, int rrn, int bonus) {
+        super(name, address, salary, rrn);
+        this.bonus = bonus;
+    }
+
+
+    public void test(){
+        System.out.println("name = " + this.name);
+        System.out.println("address = " + this.address);
+        System.out.println("salary = " + this.salary);
+    }
+
+}
