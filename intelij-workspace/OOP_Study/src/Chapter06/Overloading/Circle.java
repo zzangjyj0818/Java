d6250a97cc20f16bd64109be37060e691f5b1b68
zzangjyj0818 <jyj010818@naver.com>
2023-04-17 09:21:22 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Chapter06/Overloading/Circle.java
@@ -0,0 +1,20 @@
+package Chapter06.Overloading;
+
+public class Circle {
+
+    private int radius;
+    private Point center;
+
+    public Circle(int radius, Point center) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+}
---
+++ intelij-workspace/OOP_Study/src/Chapter06/Overloading/Point.java
@@ -0,0 +1,17 @@
+package Chapter06.Overloading;
+public class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
