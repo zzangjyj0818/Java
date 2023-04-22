7e5793c91f6dfcad4eaec03181a011586d8b5f0b
zzangjyj0818 <jyj010818@naver.com>
2023-04-22 17:40:39 +0900

    oop Study

---
+++ intelij-workspace/OOP_Study/src/Chapter07/Rectangle.java
@@ -0,0 +1,25 @@
+package Chapter07;
+
+public class Rectangle extends Shape{
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    void draw(){
+        System.out.println("(" + this.getX() + " " + this.getY() + ") " + width + " " + height);
+    }
+}
