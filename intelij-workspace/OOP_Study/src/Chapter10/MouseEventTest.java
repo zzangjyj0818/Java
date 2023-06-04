bde91a75530e808ce23e8ba92d1e9b835b1d200b
zzangjyj0818 <jyj010818@naver.com>
2023-06-04 14:39:23 +0900

    GUI

---
+++ intelij-workspace/OOP_Study/src/Chapter10/MouseEventTest.java
@@ -0,0 +1,73 @@
+package Chapter10;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+class Rectangle {
+    int x, y, w, h;
+}
+
+class MyPanel4 extends JPanel implements MouseListener {
+    BufferedImage img = null;
+    int img_x = 0, img_y = 0;
+    Rectangle[] array = new Rectangle[100];
+    int index = 0;
+
+    public MyPanel4() {
+        this.addMouseListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (Rectangle r : array) {
+            if (r != null)
+                g.drawRect(r.x, r.y, r.w, r.h);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (index > 100)
+            return;
+        array[index] = new Rectangle();
+        array[index].x = e.getX();
+        array[index].y = e.getY();
+        array[index].w = 50;
+        array[index].h = 50;
+        index++;
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+}
+
+public class MouseEventTest extends JFrame {
+    public MouseEventTest() {
+        setSize(300, 300);
+        setTitle("마우스로 사각형 그리기");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new MyPanel4());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MouseEventTest s = new MouseEventTest();
+    }
+}
--- intelij-workspace/OOP_Study/src/Chapter14/FileError.java
+++ intelij-workspace/OOP_Study/src/Chapter14/FileError.java
@@ -1,2 +1,39 @@
-package Chapter14;public class FileError {
+package Chapter14;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FileError {
+    private  int[] list;
+    private static final int SIZE = 10;
+
+    public FileError() {
+        list = new int[SIZE];
+        for(int i = 0; i<10; i++)
+            list[i] = i;
+        writeList();
+    }
+
+    public void writeList(){
+        PrintWriter out = null;
+        try{
+            out = new PrintWriter(new FileWriter("outfile.txt"));
+            for(int i = 0; i<SIZE; i++){
+                out.println("배열 원소 " + i + " = " + list[i]);
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.err.println("ArrayIndexOutOfBoundsException");
+        }catch (IOException e){
+            System.err.println("IOException");
+        }finally {
+            if(out != null)
+                out.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        new FileError();
+    }
 }
